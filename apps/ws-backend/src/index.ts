eee83aa588c137d61116a80faf1d837a8de36824
Kalpesh Borde <bordekalpesh@gmail.com>
2025-06-16 13:21:26 +0530

    added authentication layer in ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -8,6 +8,21 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function checkUser(token: string): string | null {
+    const decoded  = jwt.verify(token, JWT_SECRET);
+
+    if(typeof decoded == 'string') {
+        return null;
+    }
+
+    if(!decoded || !decoded.userId) {
+        return null;
+    }
+
+    return decoded.userId;
+
+}
+
 wss.on('connection', function connection(ws, request) {
 
     const url = request.url;
@@ -16,12 +31,11 @@ wss.on('connection', function connection(ws, request) {
         return;
     }
     const queryParams = new URLSearchParams(url.split('?')[1]);
-    const token = queryParams.get('token');
-    const decoded  = jwt.verify(token as string, JWT_SECRET);
-
-    if(!decoded || !(decoded as JwtPayload).userId) {
+    const token = queryParams.get('token') || "";
+    
+    const userAuthenticated = (checkUser(token));
+    if(!userAuthenticated) {
         ws.close();
-        return;
     }
 
     ws.on('message', function message(data) {
