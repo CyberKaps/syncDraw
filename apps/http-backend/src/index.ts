08f34954151e167b16950bd2c1ed0037f10cf64a
Kalpesh Borde <bordekalpesh@gmail.com>
2025-06-16 11:53:58 +0530

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,24 +3,39 @@ import jwt from "jsonwebtoken";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { CreateUserSchema, SignInSchema, CreateRoomSchema } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 
 const app = express();
 
 
-app.post("/signup", (req, res) => {
+app.post("/signup",async (req, res) => {
 
-    const data = CreateUserSchema.safeParse(req.body);
-    if(!data.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success) {
         res.status(400).json({
             error: "Invalid data"
         });
         return;
     }
 
-    res.json({
-        userId: "123",
-    })
+    try {
+        await prismaClient.user.create({
+            data: {
+                email: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+
+        res.json({
+            userId: "123",
+        })
+    } catch (error) {
+        res.status(411).json({
+            error: "User already exists"
+        });
+    }
 
 });
 
