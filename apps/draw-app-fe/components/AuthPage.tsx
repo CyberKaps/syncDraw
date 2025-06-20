67bca7bb794521c9b6df085c495fc0a432385f00
Kalpesh Borde <bordekalpesh@gmail.com>
2025-06-20 12:10:17 +0530

    landing page

--- apps/draw-app-fe/components/AuthPage.tsx
+++ apps/draw-app-fe/components/AuthPage.tsx
@@ -6,11 +6,10 @@ export function AuthPage({ isSignin }: {
     isSignin: boolean
 }) {
     return <div className="w-screen h-screen flex justify-center items-center">
-        <div className="p-8 m-2 bg-white rounded-2xl">
+        <div className="p-8 m-2 bg-white rounded border">
             <h1 className="text-black text-2xl text-center font-bold my-2">{isSignin ? "Sign in" : "Sign up"}</h1>
 
             <div className="p-2 text-black  rounded">
-                {/* <input type="text" placeholder="email"></input> */}
                 <Input type="text" placeholder="Email" />
             </div>
 
@@ -18,11 +17,8 @@ export function AuthPage({ isSignin }: {
                 <Input type="password" placeholder="Password" />
             </div>
 
-           <div className="p-2">
-             {/* <button className="bg-black p-1 px-2 rounded" onClick={() => {
-                
-            }}>{isSignin ? "Sign in" : "Sign up"}</button> */}
-                <Button className="bg-black p-1 px-2 rounded" onClick={()=>{
+           <div className="p-2 flex justify-center">
+                <Button className="bg-black text-white p-2 w-[100%] rounded" onClick={()=>{
                     alert("Button clicked!"); 
                 }} >{isSignin ? "Sign in" : "Sign up"}</Button>
            </div>
